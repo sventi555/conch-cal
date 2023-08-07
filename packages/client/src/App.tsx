24a2c8896cda7a3cad98a873d4cca0983c7972d5
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2023-08-07 01:17:14 -0400

    feat: add events to frontend state using the modal

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,7 +1,7 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { Calendar } from './components/Calendar';
+import { CalendarEvent } from './components/Event';
 import { EventModal } from './components/EventModal';
-import { MS_PER_HOUR } from './utils/date';
 
 /**
  * TODO
@@ -11,34 +11,20 @@ import { MS_PER_HOUR } from './utils/date';
  * - don't draw out of bounds events
  */
 
-const now = Date.now();
-const lastHour = now - (now % MS_PER_HOUR);
-
-const events = [
-  {
-    start: lastHour,
-    end: lastHour + 3600 * 1000 * 1,
-    name: 'event 1',
-  },
-  {
-    start: lastHour + 3600 * 1000 * 2,
-    end: lastHour + 3600 * 1000 * 4,
-    name: 'event 2',
-  },
-  {
-    start: lastHour + 3600 * 1000 * 12,
-    end: lastHour + 3600 * 1000 * 13,
-    name: 'event 3',
-  },
-];
-
 export const App = () => {
+  const [events, setEvents] = useState<CalendarEvent[]>([]);
   const eventModalRef = useRef<HTMLDialogElement>(null);
 
   return (
     <>
       <Calendar events={events} />
-      <EventModal dialogRef={eventModalRef} />
+      <EventModal
+        dialogRef={eventModalRef}
+        onSubmit={(event: CalendarEvent) => {
+          setEvents([...events, event]);
+          eventModalRef.current?.close();
+        }}
+      />
       <button onClick={() => eventModalRef.current?.showModal()}>
         Add Event
       </button>
--- packages/client/src/components/EventModal.tsx
+++ packages/client/src/components/EventModal.tsx
@@ -1,20 +1,60 @@
-import { Ref } from 'react';
+import { Ref, useState } from 'react';
+import { CalendarEvent } from './Event';
 
 interface EventModalProps {
   dialogRef: Ref<HTMLDialogElement>;
+  onSubmit: (event: CalendarEvent) => void;
 }
 
-export const EventModal = ({ dialogRef }: EventModalProps) => {
+export const EventModal = ({ dialogRef, onSubmit }: EventModalProps) => {
+  const [startDay, setStartDay] = useState('2023-06-08');
+  const [startTime, setStartTime] = useState('12:00');
+  const [endDay, setEndDay] = useState('2023-06-08');
+  const [endTime, setEndTime] = useState('13:00');
+
   return (
     <dialog ref={dialogRef}>
-      <form>
-        <input
-          type="date"
-          id="start-date"
-          defaultValue="2023-06-08"
-          min="1999-06-02"
-        />
-        <input type="time" id="start-time" defaultValue="12:00" />
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          onSubmit({
+            start: new Date(`${startDay}T${startTime}`).getTime(),
+            end: new Date(`${endDay}T${endTime}`).getTime(),
+            name: 'Biffus',
+          });
+        }}
+      >
+        <div>
+          <input
+            type="date"
+            id="start-date"
+            value={startDay}
+            min="1999-06-02"
+            onChange={(e) => setStartDay(e.target.value)}
+          />
+          <input
+            type="time"
+            id="start-time"
+            value={startTime}
+            onChange={(e) => setStartTime(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            type="date"
+            id="end-date"
+            value={endDay}
+            min="1999-06-02"
+            onChange={(e) => setEndDay(e.target.value)}
+          />
+          <input
+            type="time"
+            id="end-time"
+            value={endTime}
+            onChange={(e) => setEndTime(e.target.value)}
+          />
+        </div>
+        <input type="submit" value="Add Event" />
       </form>
     </dialog>
   );
