b931abb79c3f0081fc77396e8157bbd730b531e1
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2023-11-24 15:29:03 -0500

    chore: more readable hours

--- packages/client/src/components/calendar/markers.ts
+++ packages/client/src/components/calendar/markers.ts
@@ -4,6 +4,60 @@ import { spiralCoord, spiralRadius, timeToAngle } from '../../utils/spiral';
 import { P5Component } from '../p5-component';
 import { CalendarConfig } from './Calendar';
 
+const lineMarkerCoords = (time: number, config: CalendarConfig) => {
+  const theta = timeToAngle(time, config);
+
+  const outer = spiralCoord(theta);
+  const inner = spiralCoord(theta - TWO_PI);
+
+  return { inner, outer, theta };
+};
+
+interface LineMarkerProps {
+  time: number;
+  config: CalendarConfig;
+}
+
+const drawLineMarker: P5Component<LineMarkerProps> = (p5, { time, config }) => {
+  const { inner, outer } = lineMarkerCoords(time, config);
+
+  p5.stroke(0);
+  p5.fill(0);
+
+  p5.line(outer.x, outer.y, inner.x, inner.y);
+};
+
+interface TimeMarkerProps {
+  time: number;
+  config: CalendarConfig;
+}
+
+const drawTimeMarker: P5Component<TimeMarkerProps> = (p5, { time, config }) => {
+  const { inner, outer, theta } = lineMarkerCoords(time, config);
+
+  const textCoords = {
+    x: lerp(inner.x, outer.x, 0.02),
+    y: lerp(inner.y, outer.y, 0.02),
+  };
+
+  p5.noStroke();
+  p5.textSize(dist(inner, outer) / 8);
+
+  const hour = new Date(time).getHours();
+  const isAm = hour < 12;
+  const displayText = `${hour % 12 === 0 ? 12 : hour % 12} ${
+    isAm ? 'AM' : 'PM'
+  }`;
+
+  p5.push();
+  p5.translate(textCoords.x, textCoords.y);
+  p5.scale(1, -1);
+  p5.rotate(-theta);
+  p5.translate(0, -2);
+  p5.text(displayText, 0, 0);
+  p5.pop();
+};
+
 interface MarkersProps {
   config: CalendarConfig;
 }
@@ -29,59 +83,9 @@ export const drawMarkers: P5Component<MarkersProps> = (p5, { config }) => {
   }
 
   markerTimes.forEach((time) => {
-    drawMarkerLine(p5, { time, config });
-    drawMarkerTime(p5, { time, config });
+    drawLineMarker(p5, { time, config });
+    drawTimeMarker(p5, { time, config });
   });
-};
-
-const markerCoords = (time: number, config: CalendarConfig) => {
-  const theta = timeToAngle(time, config);
-
-  const outer = spiralCoord(theta);
-  const inner = spiralCoord(theta - TWO_PI);
-
-  return { inner, outer, theta };
-};
-
-interface MarkerLineProps {
-  time: number;
-  config: CalendarConfig;
-}
-
-const drawMarkerLine: P5Component<MarkerLineProps> = (p5, { time, config }) => {
-  const { inner, outer } = markerCoords(time, config);
-
-  p5.stroke(0);
-  p5.fill(0);
-
-  // marker line
-  p5.line(outer.x, outer.y, inner.x, inner.y);
-};
-
-interface MarkerTimeProps {
-  time: number;
-  config: CalendarConfig;
-}
-
-const drawMarkerTime: P5Component<MarkerTimeProps> = (p5, { time, config }) => {
-  const { inner, outer, theta } = markerCoords(time, config);
-
-  const textCoords = {
-    x: lerp(inner.x, outer.x, 0.02),
-    y: lerp(inner.y, outer.y, 0.02),
-  };
-
-  p5.noStroke();
-  p5.textSize(dist(inner, outer) / 8);
-
-  // hour text
-  p5.push();
-  p5.translate(textCoords.x, textCoords.y);
-  p5.scale(1, -1);
-  p5.rotate(-theta);
-  p5.translate(0, -2);
-  p5.text(`${new Date(time).getHours() % 12}:00`, 0, 0);
-  p5.pop();
 };
 
 interface FocusMarkerProps {
