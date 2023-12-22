dbba0e6fe4c43313ca14ad836cfc1bdab22d7a81
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2023-12-21 23:42:50 -0500

    fix: wrong import in server

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -3,8 +3,8 @@ import 'dotenv/config';
 import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
 import { cors } from 'hono/cors';
-import { toIntOrUndefined } from 'lib';
 import { eventRoutes } from './routes/event';
+import { toIntOrUndefined } from './utils/parse';
 
 const app = new Hono();
 
