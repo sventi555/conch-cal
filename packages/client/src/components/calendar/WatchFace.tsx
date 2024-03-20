22bfb859e3b0d10ec83273b654ee8e3cc9bfa44e
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2024-03-20 00:44:23 -0400

    final iteration of watch face shape

--- packages/client/src/components/calendar/WatchFace.tsx
+++ packages/client/src/components/calendar/WatchFace.tsx
@@ -1,28 +1,51 @@
-import { spiralCoord } from '../../utils/spiral';
+import { TWO_PI } from '../../utils/math';
+import { DEFAULT_SAMPLES, spiralCoord } from '../../utils/spiral';
 import { P5Component } from '../p5-component';
 import { CalendarConfig } from './Calendar';
+import { lineMarkerCoords } from './markers/utils';
+
+export const WATCH_HEIGHT = 100;
 
 interface WatchFaceProps {
   config: CalendarConfig;
+  samplesPerRotation?: number;
 }
 
 export const drawWatchFaceBorder: P5Component<WatchFaceProps> = (
   p5,
-  { config },
+  { config, samplesPerRotation = DEFAULT_SAMPLES },
 ) => {
-  const outerCoord = spiralCoord(config.angleToFocus);
-  const height = 100;
-  const lowerWidth = 190;
-  const upperWidth = 202;
+  const sampleRate = TWO_PI / samplesPerRotation;
+  const { inner, outer } = lineMarkerCoords(config.focusedTime, config);
 
   p5.stroke(0);
   p5.strokeWeight(1.4);
+  p5.noFill();
 
   p5.beginShape();
-  p5.vertex(outerCoord.x - lowerWidth, outerCoord.y);
-  p5.vertex(outerCoord.x, outerCoord.y);
-  p5.vertex(outerCoord.x, outerCoord.y + height);
-  // TODO maybe put border radius between these two
-  p5.vertex(outerCoord.x - upperWidth, outerCoord.y + height);
+
+  // bottom line
+  p5.vertex(inner.x, inner.y);
+  p5.vertex(outer.x, outer.y);
+
+  // outer curve
+  let curAngle = config.angleToFocus + sampleRate;
+  let prevCoord = outer;
+  while (true) {
+    const curCoord = spiralCoord(curAngle);
+    if (curCoord.y > outer.y + WATCH_HEIGHT) {
+      break;
+    }
+
+    p5.vertex(curCoord.x, curCoord.y);
+
+    prevCoord = curCoord;
+    curAngle += sampleRate;
+  }
+
+  // top line
+  const upperWidth = 200;
+  p5.vertex(prevCoord.x - upperWidth, prevCoord.y);
+
   p5.endShape();
 };
